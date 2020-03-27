1e511727fe08ec337a8dfcff543931e8b30f4ab3
shprexen <shprexenplus@gmail.com>
2020-03-28 00:01:59 +0300

    l

---
+++ Num4/2/MathOperations.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DllTest
+{
+    static class MathOperations
+    {
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Sum(int n, int a, int b, int c);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int NOD(int a, int b);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int NOK(int a, int b);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double Average(int n, double a, double b, double c);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Sub(int a, int b);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Mult(int a, int b);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern double Div(int a, int b);
+    }
+}
---
+++ Num4/2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DllTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(MathOperations.Average(3,4,5,6));
+
+            Console.WriteLine(MathOperations.NOD(648,114));
+            
+            Console.WriteLine(MathOperations.NOK(18,13));
+        
+        }
+    }
+}
