e1929f402344cd9e0a90f22c712c41e617b82449
shprexen <shprexenplus@gmail.com>
2020-04-29 20:34:09 +0300

    7

---
+++ C_Sharp/Num7/Project/Project/Number.cs
@@ -0,0 +1,221 @@
+﻿using System;
+
+namespace Project
+{
+    class Number : IEquatable<Number>, IComparable
+    {
+        double value;
+
+        public int Numerator
+        {
+            get;
+
+            set;
+        }
+
+        public int Denominator
+        {
+            get;
+
+            set;
+        }
+
+        public Number()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        public Number(int num, int den)
+        {
+            Numerator = num;
+            Denominator = den;
+            value = (double)num / den;
+        }
+
+        public Number(double x)
+        {
+            value = x;
+
+            if(x%10 == 0)
+            {
+                Numerator = (int)x;
+                Denominator = 1;
+            }
+
+            else
+            {
+                int i = 0;
+                while(x%10!=0)
+                {
+                    x *= 10;
+                    i++;
+                }
+
+                i--;
+                x /= 10;
+
+                Numerator = (int)x;
+                Denominator = (int)Math.Pow(10, i);
+
+                Reduce();
+            }
+        }
+
+        public static Number operator +(Number n1, Number n2)
+        {
+            Number num = new Number();
+
+            num.Numerator = n1.Numerator * n2.Denominator + n1.Denominator * n2.Numerator;
+            num.Denominator = n1.Denominator * n2.Denominator;
+            num.value = n1.value + n2.value;
+
+            num.Reduce();
+
+            return num;
+        }
+
+        public static Number operator -(Number n1, Number n2)
+        {
+            Number num = new Number();
+
+            num.Numerator = n1.Numerator * n2.Denominator -  n1.Denominator * n2.Numerator;
+            num.Denominator = n1.Denominator * n2.Denominator;
+            num.value = n1.value - n2.value;
+
+            num.Reduce();
+
+            return num;
+        }
+        public static Number operator *(Number n1, Number n2)
+        {
+            Number num = new Number();
+
+            num.Numerator = n1.Numerator * n2.Numerator;
+            num.Denominator = n1.Denominator * n2.Denominator;
+            num.value = n1.value * n2.value;
+
+            num.Reduce();
+
+            return num;
+        }
+        public static Number operator /(Number n1, Number n2)
+        {
+            Number num = new Number();
+
+            num.Numerator = n1.Numerator * n2.Denominator;
+            num.Denominator = n1.Denominator * n2.Numerator;
+            num.value = n1.value / n2.value;
+            if (n1 < 0 || n2 < 0)
+            {
+                num.Numerator = -num.Numerator;
+                num.Denominator = Math.Abs(num.Denominator);
+            }
+
+            num.Reduce();
+
+            return num;
+        }
+
+        public static bool operator <(Number n1, Number n2)
+        {
+            return n1.value < n2.value;
+        }
+
+        public static bool operator >(Number n1, Number n2)
+        {
+            return n1.value > n2.value;
+        }
+
+        public static implicit operator int(Number num)
+        {
+            return (int)num;
+        }
+
+        public static implicit operator double(Number num)
+        {
+            return num.value;
+        }
+
+        public static bool operator<=(Number n1, Number n2)
+        {
+            return n1.value <= n2.value;
+        }
+
+        public static bool operator >=(Number n1, Number n2)
+        {
+            return n1.value >= n2.value;
+        }
+
+        public static bool operator ==(Number n1, Number n2)
+        {
+            return n1.Equals(n2);
+        }
+
+        public static bool operator !=(Number n1, Number n2)
+        {
+            return !n1.Equals(n2);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (this.value > ((Number)obj).value)
+            {
+                return 1;
+            }
+
+            if (this.value < ((Number)obj).value)
+            {
+                return -1;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool Equals(Number other)
+        {
+            return value == other.value;
+        }
+
+        public override string ToString()
+        {
+            if ((value - (int)value) != 0)
+            {
+                return $"{Numerator}/{Denominator}";
+            }
+            else
+            {
+                return $"{value}";
+            }
+        }
+
+        void Reduce()
+        {
+            int NOD(int a, int b)
+            {
+                a = Math.Abs(a);
+                b = Math.Abs(b);
+                while (a!=0 && b!=0)
+                {
+                    if (a >= b)
+                        a %= b;
+
+                    else
+                        b %= a;
+                }
+
+                return a | b;
+            }
+
+            while (NOD(Numerator,Denominator)!=1)
+            {
+                int n = NOD(Numerator, Denominator);
+                Numerator /= n;
+                Denominator /= n;
+            }
+        }
+    }
+}
---
+++ C_Sharp/Num7/Project/Project/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        public static int GetNat()
+        {
+            int a;
+
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                Console.Write("Incorrect input, repeat: ");
+            }
+
+            return a;
+        }
+
+        public static int GetInt()
+        {
+
+            int a;
+
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.Write("Incorrect input, repeat: ");
+            }
+
+            return a;
+        }
+
+
+
+        public static double GetDouble()
+        {
+
+            double a;
+
+            while (!double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.Write("Incorrect input, repeat: ");
+            }
+
+            return a;
+        }
+
+        public static Number GetFraction()
+        { 
+            Console.Write("\nEnter fraction: ");
+
+            string str = Console.ReadLine();
+            while (!CheckFraction(str))
+            {
+
+                Console.Write("Incorrect input, repeat: ");
+                str = Console.ReadLine();
+            }
+
+            try
+            {
+                string[] mas = str.Split('/');
+                return new Number(int.Parse(mas[0]), int.Parse(mas[1]));
+            }
+
+            catch
+            {
+                return new Number(int.Parse(str), 1);
+            }
+        }
+
+        public static Number GetNumber()
+        {
+            Console.Write("\nEnter number: ");
+            double number = GetDouble();
+            return new Number(number);
+        }
+
+        public static Number GetRatNumber()
+        {
+            int x;
+            Console.Write("\n0 - Fraction, 1 - Number ");
+
+            
+            while (!int.TryParse(Console.ReadLine(), out x) || x < 0 || x > 1)
+            {
+                Console.Write("Incorrect input, repeat: ");
+            }
+
+            if(x == 0)
+            {
+                return GetFraction();
+            }
+
+            else
+            {
+                return GetNumber();
+            }
+        }
+
+        public static bool CheckFraction(string frac)
+        {
+            if (String.IsNullOrEmpty(frac))
+            { 
+                return false;
+            }
+
+            for (int i = 0; i < frac.Length; i++)
+            {
+                if (i == 0 && frac[i] == '-')
+                {
+                    i++;
+                }
+
+                if((frac[i] == '/' && i != frac.Length - 1))
+                {
+                    i++;
+                }
+
+                if (frac[i] == '0' && frac.Length == 1)
+                {
+                    return false;
+                }
+
+                if (!char.IsDigit(frac[i]) || (frac[i] == '0' && i == frac.Length - 1 && frac[i - 1] == '/'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number");
+            var num1 = GetRatNumber();
+
+            Console.Write("\nEnter the second number");
+            var num2 = GetRatNumber();
+
+            Console.Clear();
+            Console.WriteLine("first number: " + num1);
+            Console.WriteLine("second number: " + num2);
+
+            Console.WriteLine($"1. {num1} > {num2} = " + (num1 > num2));
+
+            Console.WriteLine($"2. {num1} < {num2} = " + (num1 < num2));
+
+            Console.WriteLine($"3. {num1} == {num2} = " + (num1 == num2));
+
+            Console.WriteLine($"4. {num1} != {num2} = " + (num1 != num2));
+
+            Console.WriteLine($"5. {num1} + {num2} = " + (num1 + num2));
+
+            Console.WriteLine($"6. {num1} - {num2} = " + (num1 - num2));
+
+            Console.WriteLine($"7. {num1} * {num2} = " + (num1 * num2));
+        }
+    }
+}
